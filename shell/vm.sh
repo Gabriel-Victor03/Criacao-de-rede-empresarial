3a7116f7c834b012129557de903ef469701b4b6e
Gabriel-Victor03 <gabriel.victor2@estudante.ifgoiano.edu.br>
2023-11-29 23:23:41 -0300

    forma mais prática de instalar docker

--- shell/vm.sh
+++ shell/vm.sh
@@ -2,21 +2,7 @@
 
 sudo apt-get update
 
-# Instale as dependências
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-
-# Adicionando a chave GPG oficial do Docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-#adicionando repositório
-echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $
-(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update
-
-# Instalando o Docker
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker.io
+sudo systemctl start docker
+sudo usermod -aG docker $USER
+sudo systemctl restart docker
